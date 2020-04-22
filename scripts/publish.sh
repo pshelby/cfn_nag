3bd95f5ad7d351d29b485a18e32d55701eae8b6b
Patrick Shelby <patrick.shelby@stelligent.com>
2020-04-22 18:28:36 +0000

    Temporarily disabling gem and Docker image creation/pushing for testing releases.

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -2,37 +2,37 @@
 set -o pipefail
 export minor_version="0.5"
 
-set +x
-if [[ -z ${rubygems_api_key} ]];
-then
-  echo rubygems_api_key must be set in the environment
-  exit 1
-fi
-if [[ -z ${docker_user} ]];
-then
-  echo docker_user must be set in the environment
-  exit 1
-fi
-if [[ -z ${docker_password} ]];
-then
-  echo docker_password must be set in the environment
-  exit 1
-fi
-if [[ -z ${docker_org} ]];
-then
-  echo $docker_org must be set in the environment
-  exit 1
-fi
-set -x
+# set +x
+# if [[ -z ${rubygems_api_key} ]];
+# then
+#   echo rubygems_api_key must be set in the environment
+#   exit 1
+# fi
+# if [[ -z ${docker_user} ]];
+# then
+#   echo docker_user must be set in the environment
+#   exit 1
+# fi
+# if [[ -z ${docker_password} ]];
+# then
+#   echo docker_password must be set in the environment
+#   exit 1
+# fi
+# if [[ -z ${docker_org} ]];
+# then
+#   echo $docker_org must be set in the environment
+#   exit 1
+# fi
+# set -x
 
 git config --global user.email "build@build.com"
 git config --global user.name "build"
 
 mkdir -p ~/.gem
 
-set +ex
-echo :rubygems_api_key: ${rubygems_api_key} > ~/.gem/credentials
-set -ex
+# set +ex
+# echo :rubygems_api_key: ${rubygems_api_key} > ~/.gem/credentials
+# set -ex
 chmod 0600 ~/.gem/credentials
 
 current_version=$(ruby -e 'tags=`git tag -l v#{ENV["minor_version"]}.*`' \
@@ -48,16 +48,16 @@ fi
 sed -i.bak "s/0\.0\.0/${new_version}/g" cfn-nag.gemspec
 
 # publish rubygem to rubygems.org, https://rubygems.org/gems/cfn-nag
-gem build cfn-nag.gemspec
-gem push cfn-nag-${new_version}.gem
+# gem build cfn-nag.gemspec
+# gem push cfn-nag-${new_version}.gem
 
 # publish docker image to DockerHub, https://hub.docker.com/r/stelligent/cfn_nag
-docker build -t $docker_org/cfn_nag:${new_version} .
-set +x
-echo $docker_password | docker login -u $docker_user --password-stdin
-set -x
-docker tag $docker_org/cfn_nag:${new_version} $docker_org/cfn_nag:latest
-docker push $docker_org/cfn_nag:${new_version}
-docker push $docker_org/cfn_nag:latest
+# docker build -t $docker_org/cfn_nag:${new_version} .
+# set +x
+# echo $docker_password | docker login -u $docker_user --password-stdin
+# set -x
+# docker tag $docker_org/cfn_nag:${new_version} $docker_org/cfn_nag:latest
+# docker push $docker_org/cfn_nag:${new_version}
+# docker push $docker_org/cfn_nag:latest
 
 echo "::set-output name=cfn_nag_version::${new_version}"
